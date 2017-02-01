cda30e36b122def46e62e43c48ccd5005af3e1a4
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2017-02-01 10:14:35 -0600

    Add x-xsrf-token to allowed headers list for pre-flight request

--- src/main/java/com/example/filters/CorsFilter.java
+++ src/main/java/com/example/filters/CorsFilter.java
@@ -49,7 +49,7 @@ public class CorsFilter extends GenericFilterBean {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, content-type");
+        response.setHeader("Access-Control-Allow-Headers", "x-xsrf-token, x-requested-with, authorization, content-type");
 
         // Pass-through pre-flight OPTIONS requests
         // so Authorization header arrives at JwtFilter
